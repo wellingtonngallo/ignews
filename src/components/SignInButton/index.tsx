8fcde1c7acb9270811f84f007914d35d6b36b882
Wellington Gallo <wellinton.gallo77@gmail.com>
2021-03-25 22:06:36 -0300

    cria componentes de botao

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,26 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button
+      className={styles.signInButton}
+      type="button"
+    >
+      <FaGithub color="#04d361"/>
+      Gallo
+      <FiX color="#737390" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button
+      className={styles.signInButton}
+      type="button"
+    >
+      <FaGithub color="#eba417"/>
+      Sign in with Github
+    </button>
+  );
+}
