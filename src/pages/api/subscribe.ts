82d4bd81f8bd9ca74967a10d3bd29cffaf0acbd6
Wellington Gallo <wellinton.gallo77@gmail.com>
2021-03-27 16:17:11 -0300

    configurando FaunaDB

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from 'next-auth/client';
+import { stripe } from '../../services/stripe';
+
+export default async (req: NextApiRequest, response: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const session = await getSession({ req });
+
+    const stripeCustomer = await stripe.customers.create({
+      email: session.user.email
+    });
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: stripeCustomer.id,
+      payment_method_types: ['card'],
+      billing_address_collection: 'required',
+      line_items: [
+        { price: 'price_1IZ4EmFFvr4ic6jg0AOHj3Hp', quantity: 1}
+      ],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL
+    })
+
+    return response.status(200).json({ sessionId: stripeCheckoutSession.id })
+  } else {
+    response.setHeader('Allow', 'POST');
+    response.status(405).end('Method not allowed');
+  }
+}
