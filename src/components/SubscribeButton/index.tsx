21490eee02c6394953ac3f9bdcdb84cd9409aad5
Wellington Gallo <wellinton.gallo77@gmail.com>
2021-10-11 21:46:11 -0300

    feat: testes de componentes

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -4,11 +4,7 @@ import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
-interface SubscribeButtonProps {
-  priceId: string;
-}
-
-export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+export function SubscribeButton() {
   const [session] = useSession();
   const router = useRouter();
 
@@ -20,9 +16,9 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     }
 
     if (session.activeSubscription) {
-        router.push('/posts');
+      router.push('/posts');
 
-        return;
+      return;
     }
 
     try {
