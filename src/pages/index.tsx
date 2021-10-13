cf6cf1bbdc0ade27a81c9a08ae083fb8512040ac
Wellington Gallo <wellinton.gallo77@gmail.com>
2021-10-13 10:31:40 -0300

    feat: Home test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import style from './home.module.scss';
 interface HomeProps {
   product: {
     priceId: string;
-    amount: number;
+    amount: string;
   }
 }
 
@@ -25,7 +25,7 @@ export default function Home({ product }: HomeProps) {
             Get access to all the publications <br />
             <span>For {product.amount} month</span>
           </p>
-          <SubscribeButton priceId={product.priceId}/>
+          <SubscribeButton/>
         </section>
         <img src="/images/avatar.svg" alt="Girl coding"/>
       </main>
